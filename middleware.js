43ee2e440a4aefb24de52d9c0b75cae80865916d
Aashita Narayanpur <140310405+aashita-n@users.noreply.github.com>
2024-02-27 23:01:59 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -8,9 +8,9 @@ const { listingSchema, reviewSchema } = require("./schema.js");
 
 module.exports.isLoggedIn = ( req, res, next) => {
     console.log(req.user);
-    if (!req.isAuthenticated()) {  //method to check user is loged in or not
+    if (!req.isAuthenticated()) {  //method to check user is logged in or not
         req.session.redirectUrl = req.originalUrl;
-        req.flash("error", "you must be loged in to create listing!");
+        req.flash("error", "you must be logged in to create listing!");
         return res.redirect("/login");
     }
         next();
